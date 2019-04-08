9f3d1af93a5a037f30d2cfcbd0f15c1c31e14db7
lsalazar1 <lmsalazar78011@gmail.com>
2019-04-07 23:12:49 -0500

    Skeleton Code created

---
+++ Sort.cpp
@@ -0,0 +1,21 @@
+#include "Sort.h"
+
+// Constructors
+Sort::Sort(char* list, int num) {
+
+}
+
+// Destructor
+Sort::~Sort() {
+
+}
+
+// call mergeSort with parameters
+void Sort::MERGESORT() {
+
+}
+
+// Return the number of items in the vector
+int  Sort::getSize() const {
+	return size;
+}
